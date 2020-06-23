5a5e18970955a4b7e61a2d505a1fe34513ca97e6
kxiaong <kxiaong@163.com>
2020-06-23 16:42:00 +0800

    bugfix

--- BinaryTree/BinaryTree.go
+++ BinaryTree/BinaryTree.go
@@ -27,22 +27,20 @@ func PreOrderTraversal(root *TreeNode){
 	PreOrderTraversal(root.Right)
 }
 
-func PreOrderIterTraversal(root *TreeNode) []interface{
-	// 先序遍历二叉树
-	// 非递归方法
-
+func PreOrderIterTraversal(root *TreeNode) []interface{}{
 	/*
-		1. 访问一个结点p
-		2. 将这个结点p压入stack
-		3. 向左子树移动 p = p.left, go to 1
-		4. 如果p为空，从stack弹出栈顶元素,访问栈顶元素的右子树p = stack.pop().Right, goto 1
-		5. stack.IsEmpty() == true and p == nil  -->  finish
-	*/
+	先序遍历二叉树, 非递归方法
 
+	1. 访问一个结点p
+	2. 将这个结点p压入stack
+	3. 向左子树移动 p = p.left, go to 1
+	4. 如果p为空，从stack弹出栈顶元素,访问栈顶元素的右子树p = stack.pop().Right, goto 1
+	5. stack.IsEmpty() == true and p == nil  -->  finish
+	*/
+	result := make([]interface{}, 0)
 	if root == nil{
-		return
+		return result
 	}
-	result := make([]interface, 0)
 	nodeStack := stack.NewStack()
 
 	for root != nil || !nodeStack.IsEmpty(){
@@ -69,27 +67,27 @@ func  InorderTraversal(root *TreeNode){
 	InorderTraversal(root.Left)
 
 	// 访问根节点
-	fmt.Println(root.Val.(int))
+	//fmt.Println(root.Val.(int))
 
 	// 递归地遍历右子树
 	InorderTraversal(root.Right)
 }
 
-func InorderIterTraversal(root *TreeNode) []interface{
-	// 中序遍历，非递归方法
+func InorderIterTraversal(root *TreeNode) []interface{}{
 	/*
-		1. 将当前结点p压入stack
-		2. 向该结点的左子树移动, p = p.Left, goto 1
-		3. 当p为空，stack弹出栈顶元素，p=stack.Pop()。访问当前结点p.Val
-		4. 向右子树移动, p = p.Right, goto 1
-	 */
+	中序遍历，非递归方法
+
+	1. 将当前结点p压入stack
+	2. 向该结点的左子树移动, p = p.Left, goto 1
+	3. 当p为空，stack弹出栈顶元素，p=stack.Pop()。访问当前结点p.Val
+	4. 向右子树移动, p = p.Right, goto 1
+	*/
+	result := make([]interface{}, 0)
 	if root == nil{
-		return
+		return result
 	}
 
-	result := make([]interface, 0)
 	nodeStack := stack.NewStack()
-
 	for root != nil || !nodeStack.IsEmpty(){
 		for root != nil{
 			nodeStack.Push(root)
@@ -115,16 +113,17 @@ func PostOrderTraversal(root *TreeNode){
 	PostOrderTraversal(root.Right)
 
 	// 访问根节点
-	fmt.Println(root.Val.(int))
+	//fmt.Println(root.Val.(int))
 }
 
-func PostOrderIterTraversal(root *TreeNode) []interface{
-	// 后序遍历，非递归方法
+func PostOrderIterTraversal(root *TreeNode) []interface{}{
+	/* 后序遍历，非递归方法
+	*/
+	result := make([]interface{}, 0)
 	if root == nil{
-		return
+		return result
 	}
 
-	result := make([]interface, 0)
 	nodes := stack.NewStack()
 	var lastVisit *TreeNode
 
@@ -141,6 +140,6 @@ func PostOrderIterTraversal(root *TreeNode) []interface{
 			root = peak.Right
 		}
 	}
-	
+
 	return result
 }
